b4268b4f40e4ec4eec0bd42fd438a5aa13cffb21
mustafalabibl <mostafalabib2004@gmail.com>
2025-05-09 00:11:41 +0300

    update CarRotate function for actual rotate angels

--- test_car_rotate/test_car_rotate.ino
+++ test_car_rotate/test_car_rotate.ino
@@ -1,12 +1,10 @@
 // === Motor Driver Pins (L298N) ===
-#define ENA 11
-#define FENA 10
+#define ENA 10
 #define IN1 13
 #define IN2 12
 #define IN3 8
 #define IN4 7
-#define ENB 6
-#define FENB 5
+#define ENB 5
 
 // === Ultrasonic Sensor ===
 #define TRIG_PIN 9
@@ -27,9 +25,9 @@
 #define OPPONENT_DISTANCE 30  // cm
 #define AVOID_DELAY 150       // ms
 
-#define TRACK_WIDTH 0.2       // in meters
+#define TRACK_WIDTH 0.3       // in meters
 #define WHEEL_DIAMETER 0.065  // in meters
-#define MOTOR_RPM 133         // RPM of the motor
+#define MOTOR_RPM 170         // RPM of the motor
 
 float distance = 0, preDistance = 1000;
 
@@ -51,48 +49,62 @@ void setup() {
 }
 
 void loop() {
-  rotateDegrees(90, 150);  // Rotate 90 degrees in place
-  delay(1000);
+  delay(3000);
+  rotateDegrees(360);  // Rotate 360 degrees in place
+  delay(3000);
+  rotateDegrees(-360);  // Rotate 360 degrees in place
+  delay(3000);
+//   rotateDegrees(45, 255);  
+//   delay(3000);
+//   rotateDegrees(135, 255); 
+//   delay(3000);
+//   rotateDegrees(225, 255); 
+//   delay(3000);
 }
+
 
 void moveCar(int leftSpeed, int rightSpeed) {
   digitalWrite(IN3, leftSpeed > 0);
   digitalWrite(IN4, leftSpeed < 0);
   analogWrite(ENB, abs(leftSpeed));
-  analogWrite(FENB, abs(leftSpeed * 133 / 170.0));
+  // int FleftSpeed=map(leftSpeed,0,255,0,255*133/170);
+  // analogWrite(FENB, abs(FleftSpeed));
 
   digitalWrite(IN1, rightSpeed > 0);
   digitalWrite(IN2, rightSpeed < 0);
   analogWrite(ENA, abs(rightSpeed));
-  analogWrite(FENA, abs(rightSpeed * 133 / 170.0));
+  // int RleftSpeed=map(rightSpeed,0,255,0,255*133/170);
+  // analogWrite(FENA, abs(RleftSpeed));
 }
-
 void stopCar() {
   moveCar(0, 0);
 }
 
-void rotateDegrees(float degrees, int speed) {
-  float arcLength = PI * TRACK_WIDTH * (degrees / 360.0);
+void rotateDegrees(float degrees) {
+  float arcLength = PI * TRACK_WIDTH * (abs(degrees) / 360.0);
   float wheelCircumference = PI * WHEEL_DIAMETER;
   float revolutions = arcLength / wheelCircumference;
   float timeSeconds = (revolutions * 60.0) / MOTOR_RPM;
-  int timeMillis = (int)(timeSeconds * 1000);
+  int timeMillis = (int)(timeSeconds * 1000*1.895);
 
   // Left wheel backward, right wheel forward for in-place rotation
-  moveCar(-speed, speed);
+  if(degrees<0)
+  moveCar(255, -255);
+  else 
+  moveCar(-255, 255);
   delay(timeMillis);
   moveCar(0, 0);  // Stop
 }
 
 void initPins() {
   pinMode(ENA, OUTPUT);
-  pinMode(FENA, OUTPUT);
+  // pinMode(FENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   pinMode(ENB, OUTPUT);
-  pinMode(FENB, OUTPUT);
+  // pinMode(FENB, OUTPUT);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(IR_LEFT, INPUT);
